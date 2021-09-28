bd07967756c95807e2d4199f50d2afb7e0ed30d7
Sanika Patil <sanikapatil1021@gmail.com>
2021-09-28 19:34:24 +0530

    Update Staircase.cpp

--- Hackerrank/Problem solving/Staircase.cpp
+++ Hackerrank/Problem solving/Staircase.cpp
@@ -1,24 +1,20 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
-{
+{                                                  
     int n;
     cin>>n;
-    for(int i=n;i>0;i--){
-      for(int j=0;j<=n;j++){
-          if (j>=i)
-        {
-          cout<<"#";
-        }
-        else
-        {
-          cout<<" ";
-        }
-      } 
-      
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+           if(j<=n-i){
+             cout<<" ";  
+           }
+         else{
+           cout<<"#";  
+         } 
+        } 
       cout<<endl;
-    }    
+    }
     return 0;
 }
