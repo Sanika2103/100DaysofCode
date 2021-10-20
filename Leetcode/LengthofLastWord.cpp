7506b25c334c186c611a27ae1bf89f3f4e91492e
Sanika Patil <sanikapatil1021@gmail.com>
2021-10-20 09:37:42 +0530

    Update LengthofLastWord.cpp

--- Leetcode/LengthofLastWord.cpp
+++ Leetcode/LengthofLastWord.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int count =0;
-        for(int i=s.length();i>0;i--){
-        if((s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122))
-        count++;
-        if(s[i]==32)
-         break;
+        
+        int i=s.length()-1;
+        while (i && s[i]==' '){
+        i--;
         }
+        int count =0;
+         for(i;i>=0;i--){
+          if(s[i]!=' ')
+            count++;
+          if(s[i]==' ')
+           break;   
+    }
+        
         return count;
 }
-};
-        
+};      
