8e24c3cd015651a81fa7781c6b17c3a3581cd69a
Sanika Patil <sanikapatil1021@gmail.com>
2022-01-13 16:16:58 +0530

    Create SLOOP.cpp

---
+++ Codechef/SLOOP.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int m, s;
+        cin >> m >> s;
+        cout << m / s << endl;
+    }
+    return 0;
+}
