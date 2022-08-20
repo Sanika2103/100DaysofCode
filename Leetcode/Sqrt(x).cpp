0be010ac3e6a9d86d272b33def1d533c483ae873
Sanika Patil <sanikapatil1021@gmail.com>
2022-08-20 11:12:12 +0530

    Update Sqrt(x).cpp

--- Leetcode/Sqrt(x).cpp
+++ Leetcode/Sqrt(x).cpp
@@ -1,1 +1,20 @@
 
+class Solution {
+public:
+    int mySqrt(int x) {
+    int s = 1;
+	int e = x;
+	int res = 0;
+	for (int i = 1; i <= x; i++) {
+		long long mid = s + (e - s) / 2;
+		long long t = mid * mid;
+		long long y = (mid + 1) * (mid + 1);
+
+		if (t <= x && y > x) return mid;
+		else if (t > x) e = mid - 1;
+		else  s = mid + 1;
+        
+    }
+        return 0;
+    }
+};
